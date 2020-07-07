7be126d6bdaaa3355f395f5b9458a3dd90ff0a18
WooSangGyu <wsg0701@naver.com>
2020-07-07 19:08:12 +0900

    backend Register and login simple Coding

--- Sangstagramapp/app/src/main/java/com/example/sangstagramapp/LoginActivity.java
+++ Sangstagramapp/app/src/main/java/com/example/sangstagramapp/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.sangstagramapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signin = (Button) findViewById(R.id.email_login_button);
+
+    }
+}
+public class Apiclient {
+    private static final String BASE_URL = "localhost:3000/api";
+    private static Retrofit retrofit;
+
+    public static Retrofit getApiClient() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
     }
 }
